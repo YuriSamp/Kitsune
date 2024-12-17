36cebef77c6a9d047282992963648f8786a6fcf2
Yuri <yurisamp123@gmail.com>
2024-12-17 12:35:27 -0300

    feat: improve general layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ede0d8',
+        secondary: '#d41b1bf3',
+      },
+    },
+  },
+  plugins: [],
+};
