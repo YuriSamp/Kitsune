72be470e7044ec9768696d64dae08406683cc188
Yuri <yurisamp123@gmail.com>
2024-12-17 12:57:45 -0300

    feat: add custom font and license

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const {fontFamily} = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -6,6 +8,9 @@ export default {
       colors: {
         primary: '#ede0d8',
         secondary: '#d41b1bf3',
+      },
+      fontFamily: {
+        sans: ['Satoshi', ...fontFamily.sans],
       },
     },
   },
