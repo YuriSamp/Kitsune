15e06415daec67f5ba355bf5bbd12e9b1027bb6b
Yuri <yurisamp123@gmail.com>
2024-12-16 23:10:59 -0300

    some improvements on general layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Palast';
+export const SITE_DESCRIPTION = 'My safe space to share what i have been consuming';
