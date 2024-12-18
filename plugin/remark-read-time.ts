636f46bd8dfe7423e1b169e00a02941aab8ce4f6
Yuri <yurisamp123@gmail.com>
2024-12-18 18:26:31 -0300

    fix:type for now

--- plugin/remark-read-time.ts
+++ plugin/remark-read-time.ts
@@ -2,7 +2,7 @@ import getReadingTime from 'reading-time';
 import {toString} from 'mdast-util-to-string';
 
 export const remarkReadingTime = () => {
-  return function (tree, {data}) {
+  return function (tree: any, {data}: {data: any}) {
     data.astro.frontmatter.readingTime = getReadingTime(toString(tree));
   };
 };
